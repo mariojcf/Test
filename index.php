d6a84ff00ffbe969736e4b5ec022d8f60d84eb05
mario <coradoj989@gmail.com>
2018-01-10 15:45:39 -0600

    Test Git

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+$name = "Mario";
+
+//$yearOfBirth = "3 de DIciembre del 2001 ";
+$yearOfBirth = 2001;
+//echo $yearOfBirth;
+//$work = "Estudiante";
+//echo  $work;
+//$Direccion = "39 calle B ";
+//echo $Direccion;
+ 
+echo "Hola " .$name .", tienes ". (2018 - 2001);
+
